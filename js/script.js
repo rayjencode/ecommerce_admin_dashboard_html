28d46d712797c7f18d3c398aecd2e78a6c98c3eb
Rayjens Code <rayjenscode@gmail.com>
2020-07-15 23:51:05 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const navBtn = document.querySelector('.header__collapse--btn');
+const nav = document.querySelector('.nav');
+const navWrapper = document.querySelector('.navWrapper');
+const navClose = document.querySelector('.nav__close');
+
+navBtn.addEventListener('click', () => {
+    console.log(`object`);
+    nav.classList.toggle('show');
+    // navWrapper.classList.toggle('show-item');
+});
+
+navClose.addEventListener('click', (e) => {
+    console.log(e.target.parentElement);
+    nav.classList.remove('show');
+    // navWrapper.classList.remove('show-item');
+});
