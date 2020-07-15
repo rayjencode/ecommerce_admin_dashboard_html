3d67e523443b0210c30b5b18b113051dabb09603
Rayjens Code <rayjenscode@gmail.com>
2020-07-16 02:21:15 +0800

    adding javascript for toggle event

--- js/script.js
+++ js/script.js
@@ -3,6 +3,17 @@ const nav = document.querySelector('.nav');
 const navWrapper = document.querySelector('.navWrapper');
 const navClose = document.querySelector('.nav__close');
 
+// Nav
+const showOrder = document.querySelector('.nav__showOrders');
+const showProducts = document.querySelector('.nav__showProducts');
+const navOrdersList = document.querySelector('.nav__orders--items');
+const navProductsList = document.querySelector('.nav__products--items');
+
+// header profile
+const toggleProfileNav = document.querySelector('.header__profile');
+const showProfileNav = document.querySelector('.header__profile__name--nav');
+
+// Actions
 navBtn.addEventListener('click', () => {
     console.log(`object`);
     nav.classList.toggle('show');
@@ -13,4 +24,15 @@ navClose.addEventListener('click', (e) => {
     console.log(e.target.parentElement);
     nav.classList.remove('show');
     // navWrapper.classList.remove('show-item');
+});
+
+showOrder.addEventListener('click', () => {
+    navOrdersList.classList.toggle('show');
+});
+showProducts.addEventListener('click', () => {
+    navProductsList.classList.toggle('show');
+});
+
+toggleProfileNav.addEventListener('click', () => {
+    showProfileNav.classList.toggle('show');
 });
